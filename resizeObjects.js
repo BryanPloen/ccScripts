f0978b98c5398d6462004f4dd65f5cafcda823ae
BryanPloen <51464417+BryanPloen@users.noreply.github.com>
2023-09-05 18:51:29 +0200

    Add files via upload

--- resizeObjects.js
+++ resizeObjects.js
@@ -1,37 +1,38 @@
 (function () {
 
+    // Initialize the document and selection
     var doc = app.activeDocument;
     var selection = doc.selection;
 
+    // Constants
+    var WIDTH_BASE = 50;
+    var SCALE_FACTOR = 0.525;
 
-    // Define the base width and scale factor
-
-    var widthBase = 50;  // Overall Size Value
-    var scaleFactor = 0.525;
-
-    /** scaleFactor values closer to 0 will result in same
-     * width and values closer to one 
-     * will result in same height  */
-
-    for (var j = 0; j < selection.length; j++) {
-
-        var itemWidth = selection[j].width;
-        var itemHeight = selection[j].height;
-        var itemRatio = itemWidth / itemHeight;
-
-        // Apply the Proportional Image Normalization Formula
-        var newWidth = Math.pow(itemRatio, scaleFactor) * widthBase;
-
-        // Calculate the new height based on the new width and original aspect ratio
-        var newHeight = newWidth / itemRatio;
-
-        // Calculate the percentage change for width and height
-        var xScale = (newWidth / itemWidth) * 100;
-        var yScale = (newHeight / itemHeight) * 100;
-
-        // Resize the item
-        selection[j].resize(xScale, yScale, true, true, true, true, 1);
+    // Main function to resize items
+    function resizeItems(selection) {
+        for (var j = 0; j < selection.length; j++) {
+            var item = selection[j];
+            var newSize = calculateNewSize(item.width, item.height);
+            resizeItem(item, newSize.newWidth, newSize.newHeight);
+        }
     }
 
+    // Function to calculate new size
+    function calculateNewSize(itemWidth, itemHeight) {
+        var itemRatio = itemWidth / itemHeight;
+        var newWidth = Math.pow(itemRatio, SCALE_FACTOR) * WIDTH_BASE;
+        var newHeight = newWidth / itemRatio;
+        return { newWidth: newWidth, newHeight: newHeight };
+    }
+
+    // Function to resize an individual item
+    function resizeItem(item, newWidth, newHeight) {
+        var xScale = (newWidth / item.width) * 100;
+        var yScale = (newHeight / item.height) * 100;
+        item.resize(xScale, yScale, true, true, true, true, 1);
+    }
+
+    // Run the main function
+    resizeItems(selection);
 
 })();
